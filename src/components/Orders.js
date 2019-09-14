2681eb38a9ebb5b1f9a3723f7930c17ec4bd460c
Igor Eskin <eskinhd@gmail.com>
2019-09-14 19:02:29 -0400

    Search field in Orders.js

--- src/components/Orders.js
+++ src/components/Orders.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import OrderView from './OrderView';
 
 const Orders = ({ orderData}) => {
 
   const renderOrders = orderData.map((order) => <OrderView key={order.id} order={order} />)
 
+  const handleOnClick = e => {
+    console.log(e.target.value)
+  }
+
   return (
-    <div className='orders'>
-      { renderOrders }
-    </div>
+    <Fragment>
+      <div className='filter'>
+        <input placeholder={`Filter by worker name... `} onClick={e => handleOnClick(e)}/>
+        <i className="fas fa-search"></i>
+      </div>
+      <div className='orders'>
+        { renderOrders }
+      </div>
+    </Fragment>
   )
 }
 
